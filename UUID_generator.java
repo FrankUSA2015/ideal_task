b301b7caf3b96453e97a57877cd1de63da7d8af0
FrankUSA2015 <2019682602@qq.com>
2021-08-10 01:19:41 +0800

    自定义UUID
    功能：
    （1）生成16位的UUID
    （2）生成全数字的8位UUID

---
+++ UUID_generator.java
@@ -0,0 +1,18 @@
+
+import java.util.UUID;
+
+public class UUID_generator {
+
+     public static  Integer getUUIDInOrderId(){
+        Integer orderId=UUID.randomUUID().toString().hashCode();
+        orderId = orderId < 0 ? -orderId : orderId; //String.hashCode() 值会为空
+        return orderId;
+    }
+
+     public static String get16UUID(){
+
+        UUID id=UUID.randomUUID();
+        String[] idd=id.toString().split("-");
+        return idd[0]+idd[1]+idd[2];
+    }
+}
